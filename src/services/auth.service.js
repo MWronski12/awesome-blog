826d0d7b1265ba67bc09e578aee0ccc53e8cb24f
MWronski12 <wronski.mikolaj@gmail.com>
2022-12-31 09:59:09 +0100

    deploying

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://my-awesome-blog-backend.ew.r.appspot.com/api/auth/";
 
 class AuthService {
   login(username, password) {
--- src/services/blog.service.js
+++ src/services/blog.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8080/api/";
+const API_URL = "https://my-awesome-blog-backend.ew.r.appspot.com/api/";
 
 class BlogService {
   getPost(id) {
